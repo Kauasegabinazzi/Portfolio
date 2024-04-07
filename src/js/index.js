10fc0887f9bf9884dc8389cf6fb5c77d3cbb2b54
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-04-06 23:36:29 -0300

    26-commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botton = document.querySelector('.btn-show-projets');
+const inative = document.querySelectorAll('.projeto: not(.ativo)');
+console.log(inative);
+botton.addEventListener('click', () => {
+
+})
