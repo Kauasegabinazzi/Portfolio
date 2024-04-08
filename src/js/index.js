60a750eb17407fa538a413ea271b1290909cea26
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-04-07 22:57:07 -0300

    28-commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,21 @@
-const botton = document.querySelector('.btn-show-projets');
-const inative = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoMostrarProjetos = document.querySelector('.btn-show-projets');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
-botton.addEventListener('click', () => {
-    inative.forEach(InativeProject => {
-        InativeProject.classList.add('ativo');
-        console.log(InativeProject.classList);
-    })
-})
+botaoMostrarProjetos.addEventListener('click', () => {
+    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+    mostrarMaisProjetos();
+
+    // Objetivo 2 - esconder o botão de mostrar mais
+    // Passo 1 - pegar o botão e esconder ele
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativos');
+    });
+}
